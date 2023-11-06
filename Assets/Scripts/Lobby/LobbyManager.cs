19f0b26d18a1d3c4b9f970c50e4023c31bc42c5e
Guillermo <guillearnautm@gmail.com>
2023-11-06 19:16:57 +0100

    lobby manager

---
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+
+	void Start()
+	{
+		NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+	}
+
+	public void CreateServer()
+	{
+		NetworkManager.Singleton.StartHost();
+		UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+	}
+
+	public void JoinServer(TextMeshProUGUI text)
+	{
+		if (text.text.Length > 1)
+		{
+			string[] @params = text.text.Split(':');
+			string IP = @params[0];
+			ushort port = ushort.Parse(@params[1]);
+
+			UnityTransport transport = NetworkManager.Singleton.GetComponent<UnityTransport>();
+
+			transport.ConnectionData.Address = IP;
+			transport.ConnectionData.Port = port;
+		}
+
+		NetworkManager.Singleton.StartClient();
+	}
+
+
+	private void OnClientConnected(ulong obj)
+	{
+		NetworkManager.Singleton.OnClientConnectedCallback -= OnClientConnected;
+
+		const string SCENE = "Main";
+		SceneManager.LoadScene(SCENE);
+	}
+
+}
